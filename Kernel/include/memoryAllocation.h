97f2fcb192db9330957c0af86f69015aa5597f78
Karahtar <npaganin@itba.edu.ar>
2017-10-30 10:13:27 -0300

    added mem allocation files, incomplete

---
+++ Kernel/include/memoryAllocation.h
@@ -0,0 +1,24 @@
+#ifndef _MEMORYALLOCATION_H_
+#define _MEMORYALLOCATION_H_
+
+struct memBlock {
+	char free;		// is there anything smaller?
+	int size;
+};
+
+struct buddyTreeNode {
+	struct memBlock block;
+	struct memBlock * left;
+	struct memBlock * right;
+};
+
+struct buddyAllocator {
+	struct buddyTree * root;
+	int size;
+};
+
+void * allocPage(int size);
+
+int freePage(void * page);
+
+#endif
---
+++ Kernel/memoryAllocation.c
@@ -0,0 +1,12 @@
+// buddy allocation
+// include memalloc.h
+
+struct buddyAllocator myAllocation;
+
+void * allocPage(int size) {
+	//
+}
+
+int freePage(void * page) {
+	return 0;
+}
