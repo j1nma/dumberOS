1a6323218b223a025441421e775e74aa52f3d967
Martina Scomazzon <martinascomazzon@Martinas-MacBook-Pro.local>
2017-11-03 18:24:42 -0300

    falta dealloc()

--- Kernel/include/memoryAllocation.h
+++ Kernel/include/memoryAllocation.h
@@ -1,37 +1,56 @@
 #ifndef _MEMORYALLOCATION_H_
 #define _MEMORYALLOCATION_H_
 
-#define NULLPTR (void*) -1
+// #define NULLPTR (void*) -1
 
-/*
-**	The char free is:
-**	0 if occupied
-**	1 if semi-free (one of the descendants is occupied)
-**	2 if free
-*/
-enum memState {OCCUPIED = 0, SEMIFREE, FREE};
+// /*
+// **	The char free is:
+// **	0 if occupied
+// **	1 if semi-free (one of the descendants is occupied)
+// **	2 if free
+// */
+// enum memState {OCCUPIED = 0, SEMIFREE, FREE};
 
-struct memBlock {
-	char free;
-	int size;
-};
+// struct memBlock {
+// 	char free;
+// 	int size;
+// };
 
-struct BuddyTreeNode {
-	struct memBlock block;
-	struct memBlock * left;
-	struct memBlock * right;
-};
+// struct BuddyTreeNode {
+// 	struct memBlock block;
+// 	struct memBlock * left;
+// 	struct memBlock * right;
+// };
 
-struct BuddyAllocator {
-	struct buddyTreeNode * root;
-	int size;
-};
+// struct BuddyAllocator {
+// 	struct buddyTreeNode * root;
+// 	int size;
+// };
 
-void * allocPage(int size);
-void * searchForSpace(int size, struct BuddyAllocator myAllocation);
-void * searchForSpaceDFS(int size, struct BuddyTreeNode treeNode);
-char isChildFree(struct BuddyTreeNode treeNode);
-char isNotLeaf(int size);
-int freePage(void * page);
+// void * allocPage(int size);
+// void * searchForSpace(int size, struct BuddyAllocator myAllocation);
+// void * searchForSpaceDFS(int size, struct BuddyTreeNode treeNode);
+// char isChildFree(struct BuddyTreeNode treeNode);
+// char isNotLeaf(int size);
+// int freePage(void * page);
+
+#define MEMORYSIZE 1024
+#define MEMORYPICES MEMORYSIZE/2
+#define PAGE_SIZE 1024*4
+
+typedef enum state {EMPTY=0, ALMOST_FULL, FULL} State;
+
+struct node{
+	void * base;
+	int level;
+	State state; 
+}; 
+
+
+void startHeap(); 
+void fillInformation (struct node * nodeToFill, int level, int elementNumber, int elementsInLevel, int nodoNumber); 
+int getLevel(int size); 
+void * allocPage (int size); 
+void * findSpace(int level, int i, int currentLevel); 
 
 #endif
